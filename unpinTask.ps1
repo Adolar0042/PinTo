80701361e8932aee3559839c3e199bcf8dedfa80
Adolar0042 <39769465+Adolar0042@users.noreply.github.com>
2022-06-03 12:34:18 +0200

    Create unpinTask.ps1

---
+++ unpinTask.ps1
@@ -0,0 +1,7 @@
+function unpinTask($appname){
+    (
+        (New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | 
+        Where-Object{$_.Name -eq $appname}).Verbs() | 
+        Where-Object{$_.Name.replace('&','') -match 'Unpin from Taskbar'} | 
+        ForEach-Object{$_.DoIt(); $exec = $true}
+}

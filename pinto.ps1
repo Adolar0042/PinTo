0d09a587740c2ca09fd1f77f8a6e2662da4d471a
Adolar0042 <39769465+Adolar0042@users.noreply.github.com>
2022-06-03 11:55:59 +0200

    Create pinto.ps1

---
+++ pinto.ps1
@@ -0,0 +1,32 @@
+function pinTo([string]$app, [string]$action) {
+    if (Test-Path $app) {
+        {
+            if (!(Test-Path "$($PSScriptRoot)\syspin.exe")) {
+                Invoke-WebRequest "http://www.technosys.net/download.aspx?file=syspin.exe" -OutFile "$($PSScriptRoot)\syspin.exe"
+            }
+            if ($action -eq "pinTask") {
+                $num = 5386
+            }
+            elseif ($action -eq "unpinTask") {
+                $num = 5387
+            }
+            elseif($action -eq "pinStart") {
+                $num = 51201
+            }
+            elseif($action -eq "unpinStart") {
+                $num = 51394
+            }
+            else {
+                Write-Host "Invalid action"
+                exit 0
+            }
+        
+            (. "$($PSScriptRoot)\syspin.exe" $app $num) 
+            
+            Remove-Item "$($PSScriptRoot)\syspin.exe" -Force
+        }
+    }
+    else {
+        Write-Host "Application not found, make sure the path is corrct and the file is a .exe or .lnk"
+    }
+}
